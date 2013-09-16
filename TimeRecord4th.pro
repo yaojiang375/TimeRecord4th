af5b7bbc1644986f38536a1f7ab68c1386b8bed4
yaojiang375 <547320073@qq.com>
2013-09-16 12:39:51 +0800

    1
    目前能够正常提取字词，下一步把XML模块加进去

---
+++ TimeRecord4th.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-09-16T09:30:53
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = TimeRecord4th
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <QCoreApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QtXml/QtXml>
+#include <QString>
+int main()
+{
+
+    QFile       csvRead("c:/1.csv");
+    csvRead.open(QIODevice::ReadOnly);
+    QTextStream textRead(&csvRead);
+    QString     Reader;
+    QString     Year,Month,Day,Time,Hour,Minute;
+    QString     Buffer;
+    QString     LastRecord,Nextrecord,LastRemember,NextRemember;
+    QString     ReadItLatter;//待办记事
+    int         DayDate,TimeDate;
+    int         LastPos,NextPos;//定义完毕，正式开始
+
+    /*****************************************/
+    Reader      = textRead.readAll();
+    //qDebug()<<Reader;//Debug;
+    LastPos     = Reader.indexOf("sms,submit,");
+    while(LastPos != -1)
+    {
+        LastPos = Reader.indexOf(",,",LastPos);
+        LastPos = Reader.indexOf(",,",LastPos+1);
+        Year    = Reader.mid(LastPos+2,4);//"sms,submit,,	10086,,2013. 6.25  9:44,109,。？做答案"
+        Month   = Reader.mid(LastPos+7,2);
+        Day     = Reader.mid(LastPos+10,2);//"  9:44,109,。？做答案""
+        Hour    = Reader.mid(LastPos+13,2);
+        Minute  = Reader.mid(LastPos+16,2);
+        NextPos = Reader.indexOf("sms,",LastPos+19);//一旦有人把sms写进信息体里面怎么办？
+        Buffer  = Reader.mid(LastPos+19+4,NextPos-LastPos-23);
+        qDebug()<<"Year="+Year;qDebug()<<"Month="+Month;qDebug()<<"Day="+Day;qDebug()<<"Hour="+Hour;qDebug()<<"Minute="+Minute;//Debug
+        qDebug()<<"Buffer = "+Buffer;//Debug
+        /*buffer处理*/
+        LastPos = Reader.indexOf("sms,submit,",NextPos);
+
+    }
+
+
+    return 0;
+}
